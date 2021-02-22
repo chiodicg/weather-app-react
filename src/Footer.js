a0632cd1899cef95d63cd285a1518ca981dd4a8c
chiodicg <71133849+chiodicg@users.noreply.github.com>
2021-02-22 21:01:52 +0000

    Fixed footer link
    -Fixed footer link

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       <footer>
-        <a href="https://github.com/chiodicg/Weather-app" target="_blank" rel='noreferrer noopener'>
+        <a href="https://github.com/chiodicg/weather-app-react" target="_blank" rel='noreferrer noopener'>
           Open-source code
         </a>{" "}
         by Carla G. Chiodi
